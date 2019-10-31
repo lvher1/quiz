0f23ef8dfb5ceaa58b7d0a10864b9459d667fd75
lvher <lvher@naver.com>
2019-10-31 21:33:26 +0900

    20191031

---
+++ app/src/main/java/com/example/quiz_coding/StandardActivity.java
@@ -0,0 +1,20 @@
+package com.example.quiz_coding;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StandardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standard);
+
+        //btn = findViewById(R.id.btn);
+
+
+    }//onCreate
+
+
+}
